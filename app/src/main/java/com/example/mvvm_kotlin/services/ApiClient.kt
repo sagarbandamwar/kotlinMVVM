c1468aef94dd89a8c0e0be1d7497c9b6b3718a13
rahul <sagarbandamwar@gmail.com>
2020-04-15 18:49:56 +0530

    Api client is added

---
+++ app/src/main/java/com/example/mvvm_kotlin/services/ApiClient.kt
@@ -0,0 +1,40 @@
+package com.example.mvvm_kotlin.services
+
+import com.example.mvvm_kotlin.model.BlogsResponse
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+object ApiClient {
+    private const val API_BASE_URL = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/"
+    private var servicesApiInterface: ServicesApiInterface? = null
+
+    fun build(): ServicesApiInterface? {
+        val builder: Retrofit.Builder = Retrofit.Builder()
+            .baseUrl(API_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+
+        val httpClient: OkHttpClient.Builder = OkHttpClient.Builder()
+        httpClient.addInterceptor(interceptor())
+
+        val retrofit: Retrofit = builder.client(httpClient.build()).build()
+        servicesApiInterface = retrofit.create(
+            ServicesApiInterface::class.java
+        )
+        return servicesApiInterface as ServicesApiInterface
+    }
+
+    private fun interceptor(): HttpLoggingInterceptor {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        return httpLoggingInterceptor
+    }
+
+    interface ServicesApiInterface {
+        @GET("facts")
+        fun museums(): Call<BlogsResponse>
+    }
+}

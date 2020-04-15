8ab9c40a3f8e340874185ba241434681e1157327
rahul <sagarbandamwar@gmail.com>
2020-04-15 18:52:06 +0530

    Resource files added for dimension colors and styles

---
+++ app/src/main/java/com/example/mvvm_kotlin/di/Injection.kt
@@ -0,0 +1,19 @@
+package com.example.mvvm_kotlin.di
+
+import androidx.lifecycle.ViewModelProvider
+import com.example.mvvm_kotlin.model.BlogDataSource
+import com.example.mvvm_kotlin.model.BlogRepository
+import com.example.mvvm_kotlin.viewmodel.ViewModelFactory
+
+object Injection {
+    private val BLOG_DATA_SOURCE: BlogDataSource = BlogRepository()
+    private val museumViewModelFactory = ViewModelFactory(BLOG_DATA_SOURCE)
+
+    fun providerRepository():BlogDataSource{
+        return BLOG_DATA_SOURCE
+    }
+
+    fun provideViewModelFactory(): ViewModelProvider.Factory{
+        return museumViewModelFactory
+    }
+}
